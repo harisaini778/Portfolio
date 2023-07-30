90fb08656489fce534ca0265720f45b767b5d8cd
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-07-30 16:53:02 +0530

    added more projects in the carausel items

--- src/components/Project.js
+++ src/components/Project.js
@@ -95,6 +95,42 @@ const Project = () => {
                   </div>
                 </Card.Body>
               </Card>
+                      </Carousel.Item>
+                      <Carousel.Item className="carousel-items-project">
+              <Card style={{ width: "100vw" }}>
+                <Card.Img variant="top" src={mealsimg} />
+                <Card.Body className="bg-black card-body-project">
+                  <Card.Title className="card-title-project">
+                    My Restaurant Website
+                  </Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <div className="d-grid">
+                    <Button
+                      variant="outline-info"
+                      size="lg"
+                      as="a"
+                      href="https://my-restaurant-website-omega.vercel.app/"
+                     target="_blank"
+                     className="m-2"                  
+                    >
+                      Check Link
+                    </Button>
+                    <Button
+                      variant="outline-info"
+                      size="lg"
+                      as="a"
+                      href="https://hari-e-commercewebsite.vercel.app/Home"
+                      target="_blank"
+                      className="m-2" 
+                    >
+                      Clone Git
+                    </Button>
+                  </div>
+                </Card.Body>
+              </Card>
             </Carousel.Item>
           </Carousel>
         </div>
