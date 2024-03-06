2e1bd91c5f9c21fc57fe9bb131341b885362b2cc
harisaini778 <harikumarsaini778@gmail.com>
2024-03-06 18:31:34 +0530

    updated the resume, also added the email service for the contact form, added new pics for the skills

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,62 +1,162 @@
-import React from "react";
+import React, { useState } from "react";
 import contactimg from "./assets/contact2.png";
-import { Container,Form,Card,Button } from "react-bootstrap";
+import { Container, Form, Card, Button } from "react-bootstrap";
 import "./Contact.css";
-import {BsMapFill,BsCardText,BsPhoneFlip,BsCircleFill,BsFillSendFill} from "react-icons/bs";
-
+import { BsMapFill, BsCardText, BsPhoneFlip, BsCircleFill, BsFillSendFill } from "react-icons/bs";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const [formValues, setFormValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    message: "",
+  });
 
-    return (
-        <div className="main-container-contact">
-            <Container className="contact-heading-container">
-                <h>Contact</h>
-            </Container>
-            <Container > 
-            <img src={contactimg} alt="contactimg" className="img1-contact"/>       
-            </Container>
-            <Container className="contact-details-container">
-                <Card className="contact-details-card">
-                            <Card.Body>
-                                <h3 className="card-body-heading" ><span className="m-4" style={{color:"aqua"}}><BsCircleFill/></span>Hari Kumar Saini</h3>
-                                <h3 className="card-body-heading"><span className="m-4" style={{color:"aqua"}}><BsCardText/></span>harikumarsaini608@gmail.com</h3>
-                                <h3 className="card-body-heading"><span className="m-4" style={{color:"aqua"}}><BsPhoneFlip/></span>+91-7985027344</h3>
-                                <h3 className="card-body-heading"> <span className="m-4" style={{color:"aqua"}}><BsMapFill/></span>Balrampur,Uttar Pradesh,India</h3>
-                            </Card.Body>
-                            </Card>        
-            </Container>
-            <Container className="contact-form-container">
-                <Container className="mt-2"><h>Connect with me.</h></Container>
-                <Form className="mt-4">
-                    <Form.Group className="mb-3 contact-form">
-                        <Form.Label className="mb-2">
-                           First Name -
-                        </Form.Label>
-                        <Form.Control type="text" placeholder="Your Fisrt Name" className="mb-2" />
-                        <Form.Label className="mb-2">
-                            Last Name -
-                        </Form.Label>
-                        <Form.Control type="text" placeholder="Your Last Name" className="mb-2"/>
-                        <Form.Label className="mb-2">
-                            Email Address -
-                        </Form.Label>
-                        <Form.Control type="email" placeholder="Your Email" className="mb-2" />
-                        <Form.Label className="mb-2">
-                            Enter Phone Number -
-                        </Form.Label>
-                        <Form.Control type="text" placeholder="Your Phone Number" className="mb-2"/>
-                    </Form.Group>
-                    <div className="d-flex justify-content-center mb-3">
-                        <Button variant="outline-info" size="lg">
-                            <Container>
-                                <span className="m-1"><BsFillSendFill/></span>
-                                <span className="m-1">Send Message</span>
-                           </Container>
-                        </Button>
-                    </div>
-                </Form>
-       </Container>
-        </div>
-    );
-}
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    const emailParams = {
+      from_name: `${formValues.firstName} ${formValues.lastName}`,
+      to_name: "Hari Kumar Saini",
+      email: formValues.email,
+      phone: formValues.phoneNumber,
+      message: formValues.message,
+    };
+
+    emailjs
+      .send("service_n26knlr", "template_1p7ry3c", emailParams, "uPZlUBw-tprQkeXfX")
+      .then((response) => {
+        console.log("Email sent successfully:", response);
+        alert("Thank you for contacting me. Your message sent successfully!");
+        setFormValues({
+          firstName: "",
+          lastName: "",
+          email: "",
+          phoneNumber: "",
+          message: "",
+        });
+      })
+      .catch((error) => {
+        console.error("Email failed to send:", error);
+        alert("Failed to send message. Please try again later.");
+      });
+  };
+
+  const handleInputChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div className="main-container-contact">
+      <Container className="contact-heading-container">
+        <h>Contact</h>
+      </Container>
+      <Container>
+        <img src={contactimg} alt="contactimg" className="img1-contact" />
+      </Container>
+      <Container className="contact-details-container">
+        <Card className="contact-details-card">
+          <Card.Body>
+            <h3 className="card-body-heading">
+              <span className="m-4" style={{ color: "aqua" }}>
+                <BsCircleFill />
+              </span>
+              Hari Kumar Saini
+            </h3>
+            <h3 className="card-body-heading">
+              <span className="m-4" style={{ color: "aqua" }}>
+                <BsCardText />
+              </span>
+              harisaini607@gmail.com
+            </h3>
+            <h3 className="card-body-heading">
+              <span className="m-4" style={{ color: "aqua" }}>
+                <BsPhoneFlip />
+              </span>
+              +91-7985027344
+            </h3>
+            <h3 className="card-body-heading">
+              {" "}
+              <span className="m-4" style={{ color: "aqua" }}>
+                <BsMapFill />
+              </span>
+              Balrampur, Uttar Pradesh, India
+            </h3>
+          </Card.Body>
+        </Card>
+      </Container>
+      <Container className="contact-form-container">
+        <Container className="mt-2">
+          <h>Connect with me.</h>
+        </Container>
+        <Form className="mt-4" onSubmit={sendEmail}>
+          <Form.Group className="mb-3 contact-form">
+            <Form.Label className="mb-2">First Name -</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Your First Name"
+              className="mb-2 form-control"
+              name="firstName"
+              value={formValues.firstName}
+              onChange={handleInputChange}
+            />
+            <Form.Label className="mb-2">Last Name -</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Your Last Name"
+              className="mb-2 form-control"
+              name="lastName"
+              value={formValues.lastName}
+              onChange={handleInputChange}
+            />
+            <Form.Label className="mb-2">Email Address -</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Your Email"
+              className="mb-2 form-control"
+              name="email"
+              value={formValues.email}
+              onChange={handleInputChange}
+            />
+            <Form.Label className="mb-2">Enter Phone Number -</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Your Phone Number"
+              className="mb-2 form-control"
+              name="phoneNumber"
+              value={formValues.phoneNumber}
+              onChange={handleInputChange}
+            />
+            <Form.Label className="mb-2">Message -</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              placeholder="Your Message"
+              className="mb-2 form-control"
+              name="message"
+              value={formValues.message}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+          <div className="d-flex justify-content-center mb-3">
+            <Button type="submit" variant="outline-info" size="lg">
+              <Container>
+                <span className="m-1">
+                  <BsFillSendFill />
+                </span>
+                <span className="m-1">Send Message</span>
+              </Container>
+            </Button>
+          </div>
+        </Form>
+      </Container>
+    </div>
+  );
+};
+
 export default Contact;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,81 +1,229 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./Skills.css";
 import { Card } from "react-bootstrap";
-import { Container } from "react-bootstrap";
+import { Container,Row,Col } from "react-bootstrap";
 import skillsimg from "./assets/skills.png";
+import htmlimg from "./assets/html.png";
+import cssimg from "./assets/css.png";
+import jsimg from "./assets/JavaScript.png";
+import jqimg from "./assets/jq.png";
+import bsimg from "./assets/bootstrap.png";
+import reactbs from "./assets/reactbs.png";
+import reactimg from "./assets/react.png";
+import reduxpng from "./assets/redux.png";
+import nextimg from "./assets/next.png"
+import monngodbimg from "./assets/mongodb.png";
+import firebaseimg from "./assets/firebase.png"
+import mysqlpng from "./assets/mysqlimg.png"
+import awsimg from "./assets/awspng.png";
+import nodejsimg from "./assets/nodejs.png";
+import expressimg from "./assets/expressjs.png";
+import sequelizeimg from "./assets/sequelizeimg.png";
+import javaimg from "./assets/java.png";
+import { Stack } from "react-bootstrap/esm";
 
 const Skills = () => {
   const skillsData = [
-    { name: 'HTML', percentage: 99 },
-    { name: 'CSS', percentage: 98 },
-    {name: 'Java',percentage:98},
-    { name: 'Java Script', percentage: 95 },
-    {name: 'jQuery',percentage:98},
-    {name: 'Bootstrap',percentage:98},
-    {name: 'React', percentage: 90 },
-    { name: 'React-Bootstrap', percentage: 98 },
-    { name: 'Next Js', percentage: 98 },
-    { name: 'Bootstrap', percentage: 98 },
-    { name: 'Firebase', percentage: 98 },
+    { name: 'HTML', percentage: 99, image : htmlimg },
+    { name: 'CSS', percentage: 98,image : cssimg },
+    { name: 'Java Script', percentage: 95,image : jsimg },
+    {name: 'jQuery',percentage:98, image : jqimg},
+    {name: 'Bootstrap',percentage:98, image : bsimg},
+    {name: 'React Js', percentage: 90, image : reactimg },
+    { name: 'React-Bootstrap', percentage: 98 , image : reactbs},
+    {name: 'Redux',percentage:98, image : reduxpng},]
+
+
+   const skillsData2 = [{ name: 'Next Js', percentage: 98, image:nextimg },
+    { name: 'MongoDb', percentage: 98, image : monngodbimg },
+    { name: 'Firebase', percentage: 98, image : firebaseimg },
+    { name: 'MySql', percentage: 98, image : mysqlpng },
+    {name : "AWS",percentage:90, image : awsimg},
+    {name : "NodeJs", percentage : 96, image : nodejsimg},
+    {name : "Express", percentage : 95, image : expressimg},
+    {name : "Sequelize", percentage : 95, image : sequelizeimg},
+    // {name: 'Java',percentage:98, image :  javaimg}
   ];
 
   const [initialized, setInitialized] = useState(false);
+  const [initialized2, setInitialized2] = useState(false);
   const skillsRef = useRef(null);
+  const skillsRef2 = useRef(null);
 
   useEffect(() => {
-    const skillsObserver = new IntersectionObserver(
-      (entries) => {
-        const [entry] = entries;
-        setInitialized(entry.isIntersecting);
-      },
-      { threshold: 0.5 }
-    );
+    const handleScroll = () => {
+      const offsetTop = skillsRef.current?.offsetTop;
+      const windowHeight = window.innerHeight;
+      const scrollY = window.scrollY;
 
-    if (skillsRef.current) {
-      skillsObserver.observe(skillsRef.current);
-    }
-
-    return () => {
-      if (skillsRef.current) {
-        skillsObserver.unobserve(skillsRef.current);
+      if (offsetTop && scrollY > offsetTop - windowHeight && !initialized) {
+        setInitialized(true);
       }
     };
-  }, []);
 
-  return (
-    <div className="bg-black">
+    window.addEventListener("scroll", handleScroll);
 
-      <h className="skill-card-header">Skills</h>
-      <Container>
-        <img src={skillsimg} alt="skills" className="img1-skill"/>
-      </Container>
-        
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [initialized]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offsetTop = skillsRef2.current?.offsetTop;
+      const windowHeight = window.innerHeight;
+      const scrollY = window.scrollY;
+
+      if (offsetTop && scrollY > offsetTop - windowHeight && !initialized2) {
+        setInitialized2(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [initialized2]);
+
+  // const [initialized, setInitialized] = useState(false);
+  // const [initialized2, setInitialized2] = useState(false);
+  // const skillsRef = useRef(null);
+  // const skillsRef2 = useRef(null);
+
+  // useEffect(() => {
+  //   const skillsObserver = new IntersectionObserver(
+  //     (entries) => {
+  //       const [entry] = entries;
+  //       setInitialized(entry.isIntersecting);
+  //     },
+  //     { threshold: 0.9 }
+  //   );
+
+  //   if (skillsRef.current) {
+  //     skillsObserver.observe(skillsRef.current);
+  //   }
+
+  //   return () => {
+  //     if (skillsRef.current) {
+  //       skillsObserver.unobserve(skillsRef.current);
+  //     }
+  //   };
+  // });
+
+  // useEffect(() => {
+  //   const skillsObserver2 = new IntersectionObserver(
+  //     (entries) => {
+  //       const [entry] = entries;
+  //       setInitialized2(entry.isIntersecting);
+  //     },
+  //     { threshold: 0.9 }
+  //   );
+
+  //   if (skillsRef2.current) {
+  //     skillsObserver2.observe(skillsRef2.current);
+  //   }
+
+  //   return () => {
+  //     if (skillsRef2.current) {
+  //       skillsObserver2.unobserve(skillsRef2.current);
+  //     }
+  //   };
+  // },);
+
+//  
+
+
+return (
+  <div className="bg-black">
+
+    <h className="skill-card-header">Skills</h>
+    <Container>
+      <img src={skillsimg} alt="skills" className="img1-skill"/>
+    </Container>
+      
+  <Container>
     
-    <div className="skill-outer-div">
-     
-          <Card className="skill-card bg-black border-info">
-              <Card.Body>
-            {skillsData.map((skill, index) => (
-        <div key={index} className="skills-title">
-          {skill.name}
-          <div ref={skillsRef} className="skills-box">
-            <div className="fill" style={{ width: initialized ? `${skill.percentage}%` : 0 }}>
-              {`${skill.percentage}%`}
-            </div>
-          </div>
-        </div>
-      ))}
-              </Card.Body>
-              <Card.Footer>
-                  
-              </Card.Footer>     
-              
-          </Card>
+    <Row>
+      <Col sm={12} lg={6}
+      >
+          
+          <div className="skill-outer-div">
+   
+   <Card className="skill-card bg-black border-info">
+       <Card.Body>
+     {skillsData.map((skill, index) => (
+ <div key={index} className="skills-title">
+
+   <Stack direction="horizontal" gap={2} className="stack-container">
+    <div className="skill-name"> {skill.name}</div>
+    <div>
+    <img src={skill.image} alt={skill.name} className="skill-image" />
+    </div>
+   </Stack>
+
+  
+   <div ref={skillsRef} className="skills-box">
+     <div className="fill" style={{ width: initialized ? `${skill.percentage}%` : 0 }}>
+       {`${skill.percentage}%`}
+     </div>
+   </div>
+ </div>
+))}
+       </Card.Body>
+       <Card.Footer>
+           
+       </Card.Footer>     
+       
+   </Card>
 
 
-      </div>
-      </div>
-  );
+</div>
+
+      </Col>
+
+      <Col sm={12} lg={6}>
+
+
+      <div className="skill-outer-div">
+   
+   <Card className="skill-card bg-black border-info">
+       <Card.Body>
+     {skillsData2.map((skill, index) => (
+ <div key={index} className="skills-title">
+
+   <Stack direction="horizontal" gap={2} className="stack-container">
+    <div className="skill-name"> {skill.name}</div>
+    <div>
+    <img src={skill.image} alt={skill.name} className="skill-image" />
+    </div>
+   </Stack>
+
+  
+   <div ref={skillsRef} className="skills-box">
+     <div className="fill" style={{ width: initialized ? `${skill.percentage}%` : 0 }}>
+       {`${skill.percentage}%`}
+     </div>
+   </div>
+ </div>
+))}
+       </Card.Body>
+       <Card.Footer>
+           
+       </Card.Footer>     
+       
+   </Card>
+
+
+</div>
+        
+      </Col>
+    </Row>
+    
+    </Container> 
+ 
+    </div>
+);
 };
 
 export default Skills;
