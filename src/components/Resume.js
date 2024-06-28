9530adcc16469c588b24cf6dcb3217b2957fdf04
harisaini778 <harikumarsaini778@gmail.com>
2024-06-28 22:07:50 +0530

    updated the resume link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -21,7 +21,7 @@ const Resume = () => {
                             <Stack direction="horizontal">
                                 <div>
                                     <Container className="m-2">
-                                        <a href="https://drive.google.com/file/d/12PiMCG2-MptIh5enrHwP6bfpK1-rzchu/view?usp=sharing"
+                                        <a href="https://drive.google.com/file/d/1L4_BBu2bf7Eem9f-tRQeNWpul7VIlfMp/view?usp=sharing"
                                               target="_blank"
                                             rel="noopener noreferrer" className="link-resume">
                                             <h2>Download Resume</h2>
